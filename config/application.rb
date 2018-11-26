6cee01da79d63937f53268c0fadfb6ff20aa3718
Max Goodman <max.goodman@flatironschool.com>
2018-11-25 21:58:19 -0500

    for every generator settings

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,14 @@ Bundler.require(*Rails.groups)
 module GeneratorApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.generators do |g|
+        g.orm :active_record
+        g.template_engine :erb
+        g.test_framework :test_unit, fixture: false
+        g.stylesheets false
+        g.javascripts false
+    end
+
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
